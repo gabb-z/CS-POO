d9072ea8be0a26f30d881863e04779986eb8b767
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-21 22:03:26 -0300

    Poo1 - Lados do Triângulo

---
+++ Poo1/Poo1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            double p, areaX, areaY;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.ladoA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.ladoB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.ladoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            y.ladoA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.ladoB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.ladoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            p = (x.ladoA + x.ladoB + x.ladoC) / 2.0;
+            areaX = Math.Sqrt(p * (p - x.ladoA) * (p - x.ladoB) * (p - x.ladoC));
+
+            p = (y.ladoA + y.ladoB + y.ladoC) / 2.0;
+            areaY = Math.Sqrt(p * (p - y.ladoA) * (p - y.ladoB) * (p - y.ladoC));
+
+            Console.WriteLine("Área de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
---
+++ Poo1/Poo1/Triangulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poo1
+{
+    class Triangulo
+    {
+        public double ladoA;
+        public double ladoB;
+        public double ladoC;
+    }
+}
