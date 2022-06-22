0979eb160b780ecb6148f34c5ac4bd226b477281
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-21 22:43:23 -0300

    Poo2 - Pessoa Mais Velha

---
+++ Poo2/Poo2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Poo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1, p2;
+
+            p1 = new Pessoa();
+            p2 = new Pessoa();
+
+            Console.WriteLine("Dados da 1ª pessoa:");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da 2ª pessoa:");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
+        }
+    }
+}
