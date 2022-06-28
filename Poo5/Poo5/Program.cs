56c23afe4054cb96172c8591075eefb75115652d
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-27 23:06:43 -0300

    Poo5 - Medidas do Retângulo

---
+++ Poo5/Poo5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo();
+
+            Console.WriteLine("Digite a largura e altura do retângulo:");
+            x.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("ÁREA: " + x.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO: " + x.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL: " + x.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
