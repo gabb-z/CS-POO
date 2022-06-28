b1a937c78b0162467201fedbb89262ab04b9bb4d
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-27 22:23:18 -0300

    Poo4 - Características do Produto

---
+++ Poo4/Poo4/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo4
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidade(s), Total: R$" + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Poo4/Poo4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDados do produto: " + p);
+
+            Console.Write("\nDigite o número de produtos a ser adicionado ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qtde);
+
+            Console.Write("\nDigite o número de produtos a ser removido do estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qtde);
+
+            Console.WriteLine("\nDados atualizados: " + p);
+        }
+    }
+}
