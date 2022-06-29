9cc8879c0429e90f848c303e0ddcd7c24d2eb542
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-28 22:00:40 -0300

    Poo6 - Dados do Funcionário

---
+++ Poo6/Poo6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("\nFuncionário: " + f.Nome + ", R$ " + f.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("\nDigite a porcentagem para aumentar o salario: ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(porcent);
+
+            Console.WriteLine("\nDados atualizados: " + f.Nome + ", R$ " + f.SalarioBruto.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
