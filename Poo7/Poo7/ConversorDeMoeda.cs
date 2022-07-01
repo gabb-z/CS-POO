75a3c16870ddada54b418a9c1d8844727afdfd4a
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-30 22:55:56 -0300

    Poo7 - Conversor de Dólar para Real

---
+++ Poo7/Poo7/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Poo7
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double DolarParaReal(double quant, double cot)
+        {
+            double total = quant * cot;
+            return total + (total * Iof) / 100;
+        }
+    }
+}
---
+++ Poo7/Poo7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a cotação do dólar: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quanto dólares deseja comprar: ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.DolarParaReal(quantidade, cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais: R$ " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
